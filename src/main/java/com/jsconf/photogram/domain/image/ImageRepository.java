5859e0c483bdba8ae7c82d444bf04330bb8b1b06
JSConF <arare30030@naver.com>
2023-03-05 10:21:20 +0900

    스토리 페이지 완료

--- src/main/java/com/jsconf/photogram/domain/image/ImageRepository.java
+++ src/main/java/com/jsconf/photogram/domain/image/ImageRepository.java
@@ -1,6 +1,15 @@
 package com.jsconf.photogram.domain.image;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Integer> {
+
+    @Query(value = "SELECT * FROM image WHERE userId IN (SELECT toUserId FROM subscribe WHERE fromUserId = :principalId)", nativeQuery = true)
+    Page<Image> mStory(@Param("principalId") int principalId, Pageable pageable);
 }
--- src/main/java/com/jsconf/photogram/service/ImageService.java
+++ src/main/java/com/jsconf/photogram/service/ImageService.java
@@ -6,12 +6,15 @@ import com.jsconf.photogram.domain.image.ImageRepository;
 import com.jsconf.photogram.web.dto.image.ImageUploadDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -22,6 +25,12 @@ public class ImageService {
 
     @Value("${file.path}")
     private String uploadFolder;
+
+    @Transactional(readOnly = true) // 영속성 컨텍스트 변경 감지를 해서, 더티체킹, flush(반영) X
+    public Page<Image> imageStory(int principalId, Pageable pageable) {
+        Page<Image> images = imageRepository.mStory(principalId, pageable);
+        return images;
+    }
 
     @Transactional
     public void ImageUpload(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails){
